0e018f1e58d5011ce2e98fd95a6c633067e6b153
Denis Feklushkin <feklushkin.denis@gmail.com>
2023-10-16 03:29:28 +0300

    loopOverAllNibbles works, but without carry

--- control.sv
+++ control.sv
@@ -31,13 +31,67 @@ endmodule
 module loopOverAllNibbles
     (
         input wire clk,
-        input wire[31:0] word,
-        output wire[3:0] nibble
+        input wire[31:0] word1,
+        input wire[31:0] word2,
+        output wire[31:0] result
     );
 
-    counter #(8) curr_nibble(reset, clk, val);
+    localparam CNT_SIZE = 3;
+    logic[CNT_SIZE-1:0] curr_nibble;
+    logic reset;
 
-    always_ff @(posedge clk) begin
+    counter #(CNT_SIZE) nibble_counter(reset, clk, curr_nibble);
+
+    logic[3:0] d1;
+    logic[3:0] d2;
+    bit carry_in;
+    logic carry_out;
+    AluCtrl ctrl;
+    wire[3:0] res;
+
+    alu a(.*);
+
+    for(genvar i = 0; i <= 7; i++) begin: muxed
+        wire[3:0] src1 = word1[i*4+3:i*4];
+        wire[3:0] src2 = word2[i*4+3:i*4];
+        wire[3:0] dst;
+
+        assign result[i*4+3:i*4] = dst;
+    end
+
+    // MUX
+    always_comb
+        unique case(curr_nibble)
+            0: begin; d1 = muxed[0].src1; d2 = muxed[0].src2; muxed[0].dst = res; end
+            1: begin; d1 = muxed[1].src1; d2 = muxed[1].src2; muxed[1].dst = res; end
+            2: begin; d1 = muxed[2].src1; d2 = muxed[2].src2; muxed[2].dst = res; end
+            3: begin; d1 = muxed[3].src1; d2 = muxed[3].src2; muxed[3].dst = res; end
+            4: begin; d1 = muxed[4].src1; d2 = muxed[4].src2; muxed[4].dst = res; end
+            5: begin; d1 = muxed[5].src1; d2 = muxed[5].src2; muxed[5].dst = res; end
+            6: begin; d1 = muxed[6].src1; d2 = muxed[6].src2; muxed[6].dst = res; end
+            7: begin; d1 = muxed[7].src1; d2 = muxed[7].src2; muxed[7].dst = res; end
+        endcase
+endmodule
+
+module loopOverAllNibbles_test;
+    logic clk;
+    logic[31:0] word1;
+    logic[31:0] word2;
+    logic[31:0] result;
+
+    loopOverAllNibbles l(.*);
+
+    initial begin
+        $monitor("clk=%b w1=%h w2=%h nibble_num=%h result=%h %b", clk, word1, word2, l.curr_nibble, result, result);
+
+        clk = 0;
+        word1 = 'hfffe;
+        word2 = 1;
+
+        repeat (20) begin
+            #1
+            clk = ~clk;
+        end
     end
 endmodule
 
@@ -46,10 +100,8 @@ module counter
     (
         input wire reset,
         input wire clk,
-        output wire[WIDTH-1:0] val
+        output logic[WIDTH-1:0] val
     );
-
-    logic[WIDTH-1:0] val;
 
     always_ff @(posedge clk)
         val++;
