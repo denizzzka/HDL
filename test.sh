eaddb674510a427cd5d2e1cb4afb263ea2967dfb
Denis Feklushkin <feklushkin.denis@gmail.com>
2023-10-23 02:28:25 +0300

    forgotto add new tests execution path

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ set -euxo pipefail
 
 verilator +1800-2017ext+sv --assert --cc --build --exe --main --timing --trace --top-module tests tests.sv alu.sv instr_decoder.sv control.sv
 
-./obj_dir/VloopOverAllNibbles_test
+./obj_dir/Vtests
