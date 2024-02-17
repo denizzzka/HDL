f9f1aca7bb3a854d7864291fa65297ba57d90bd9
Denis Feklushkin <feklushkin.denis@gmail.com>
2024-02-18 00:15:19 +0300

    ALU nibbles loop goes wrong

--- tests.sv
+++ tests.sv
@@ -4,7 +4,7 @@ module tests;
     loopOverAllNibbles_test lan;
     Ram_test r;
     control_test c;
-    //~ control_test_bench tb;
+    control_test_bench tb;
 endmodule
 
 // TODO: implement tests for all instructions
@@ -20,46 +20,42 @@ module control_test_bench;
 
     TestCmd cmdsToTest[] =
     {
-        s, // addi x5, x1, 123
-        '{instr: 'h_07b08293, ret_must_be: 0}
+        s // addi x5, x1, 123
+        //~ '{instr: 'h_07b08293, ret_must_be: 123} //FIXME
     };
 
+    // all commands starting from this address
+    localparam start_addr = 32'h_ff0004;
+    //~ localparam start_addr = 32'h_ff000d;
     logic[7:0] clk_count;
-    control c(clk_count[0]);
+    control #(.START_ADDR(start_addr)) c(clk_count[0]);
 
     TestCmd cmd;
 
     initial begin
-        // all commands starting from this address
-        localparam start_addr = 32'h_ff0004;
-
         foreach(cmdsToTest[i])
         begin
             clk_count = 0;
             cmd = cmdsToTest[i];
 
-            // preload instruction
+            // Place instruction into RAM
             c.memWrite32(start_addr, cmd.instr);
 
-            // reset all registers
-            c.pc = start_addr;
+            // Initial CPU state
+            c.currState = RESET;
 
-            foreach(c.register_file[i])
-                c.register_file[i] = 0;
-
-            c.currState = INSTR_FETCH;
-
-            $monitor("clk=%b clk_count=%h state=%s opCode=%s pc=%h instr=%h", c.clk, clk_count, c.currState.name, c.opCode.name, c.pc, c.instr);
+            $monitor("clk=%b clk_count=%0d state=%s opCode=%s pc=%h instr=%h alu_perm_to_count=%b busy=%b overfl=%b loop_nibbles_number=%h nibble=%h alu_result=%h", c.clk, clk_count, c.currState.name, c.opCode.name, c.pc, c.instr, c.alu_perm_to_count, c.l.busy, c.l.overflow, c.loop_nibbles_number, c.l.curr_nibble_idx, c.alu_result);
 
             do begin
-                assert(clk_count < 100); else $error("clk_count exceeded");
+                assert(c.currState != ERROR);
+                assert(clk_count < 20); else $error("clk_count exceeded");
 
                 #1 clk_count++;
             end while(!(c.currState == INSTR_FETCH && c.pc != start_addr && c.clk == 0));
 
             // command done, check result
             // rd is always x5
-            assert(c.register_file[5] == cmd.ret_must_be); else $error("rd=%h but expected %h", c.register_file[5], cmd.ret_must_be);
+            assert(c.register_file[5] == cmd.ret_must_be); else $error("Test #%0d: rd=%h but expected %h", i, c.register_file[5], cmd.ret_must_be);
 
             assert(c.pc == start_addr + 4); else $error("%h", c.pc);
         end
